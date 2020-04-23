d6e152f158eba129de10f003948def5405e9ebe6
Matt <matt@colyer.name>
2020-04-23 13:54:28 +0000

    Undo fancy JS

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import { get } from 'axios';
+const express = require('express');
+const axios = require('axios');
 const app = express()
 const port = 3000
 
 const ASCII_ENDPOINT = "https://api.github.com/octocat";
 
 const getData = async function() {
-  const response = await get(`${ASCII_ENDPOINT}`);
+  const response = await axios.get(`${ASCII_ENDPOINT}`);
   return response.data;
 };
 
