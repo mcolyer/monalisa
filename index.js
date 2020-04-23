728d7c2c2ea353bb3ab5031131f2abb5b585b327
Matt Colyer <mcolyer@github.com>
2020-04-22 17:41:45 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const axios = require('axios')
+const app = express()
+const port = 3000
+
+const ASCII_ENDPOINT = "https://api.github.com/octocat";
+
+const getData = async function() {
+  const response = await axios.get(`${ASCII_ENDPOINT}`);
+  console.log(response.data)
+  return response.data;
+};
+
+app.get('/', async (req, res) => {
+  const output = `<pre>${await getData()}</pre>`
+  res.send(output);
+});
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
