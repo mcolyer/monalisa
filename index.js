54c4a89c4c0c74555f472d58bda3680a1ea14e0a
Matt <matt@colyer.name>
2020-04-23 16:42:49 +0000

    Make error more visible

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const axios = require('axios');
 const app = express()
 const port = 3000
 
-const ASCII_ENDPOINT = "https://api.github.com/octocat";
+const ASCII_ENDPOINT = "https://api.github.com/octocat/404";
 
 const getData = async function() {
-  const response = await axios.get(`${ASCII_ENDPOINT}`);
-  return response.status;
+  try {
+    const response = await axios.get(`${MONA_ENDPOINT}`);
+    return response.data;
+  } catch (e) {
+    const response = await axios.get(`http://artii.herokuapp.com/make?text=Not+found`);
+    return response.data;
+  }
 };
 
 app.get('/', async (req, res) => {
