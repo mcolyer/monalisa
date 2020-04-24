60cce6ecf871bf5db5508bd19dc9693314718199
Matt Colyer <matt@colyer.name>
2020-04-24 20:20:08 +0000

    New commit

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const axios = require('axios');
 const app = express()
 const port = 3000
 
-const MONA_ENDPOINT = "https://api.github.com/octocat";
+const MONA2_ENDPOINT = "https://api.github.com/octocat";
 
 const getData = async function() {
   try {
-    const response = await axios.get(`${MONA_ENDPOINT}`);
+    const response = await axios.get(`${MONA2_ENDPOINT}`);
     return response.data;
   } catch (e) {
     const response = await axios.get(`http://artii.herokuapp.com/make?text=Not+found`);
